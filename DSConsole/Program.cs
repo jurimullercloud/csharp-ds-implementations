cdbd7bfd6385842f2d60c6ac5d7b0bbef7642887
Mirzakhan Aliyev <mirzakhan.aliyev@novum.tech>
2023-11-21 10:31:05 +0400

    Implement BST build
     - Added basic Tree dump method, to visualize tree

---
+++ DSConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Trees;
+
+var avlTree = new AVLTree<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+Console.WriteLine(avlTree.Dump());
